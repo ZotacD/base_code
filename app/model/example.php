7d6d359e96fdf0930354ff609d32692924f17972
ZotacD <tom.delangle@outlook.fr>
2024-03-20 16:06:21 +0100

    Add bdd example

--- app/model/example.php
+++ app/model/example.php
@@ -1,6 +1,14 @@
 <?php
 require_once($_ENV["BASE_DIR"] . "/db-config.php");
 
+function getUsers() {
+    $bdd = db_connect();
+    $bddQuery = $bdd->prepare("SELECT * FROM USER");
+    $bddQuery->execute();
+    $users = $bddQuery->fetchAll(PDO::FETCH_ASSOC);
+    return $users;
+}
+
 function addUser()
 {
 
@@ -8,7 +16,7 @@ function addUser()
 
 function updateUser()
 {
-
+    
 }
 
 function removeUser()
--- script.sql
+++ script.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS TodoList;
 CREATE DATABASE TodoList;
 USE TodoList;
 
-CREATE TABLE USERS (
+CREATE TABLE USER (
     id_user SMALLINT AUTO_INCREMENT PRIMARY KEY,
     fname_user VARCHAR(100),
     name_user VARCHAR(100),
@@ -10,22 +10,22 @@ CREATE TABLE USERS (
     password_user VARCHAR(255)
 );
 
-CREATE TABLE TODOLIST (
-    id_todo SMALLINT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE TASK (
+    id_task SMALLINT AUTO_INCREMENT PRIMARY KEY,
     id_user SMALLINT,
-    title_todo VARCHAR(100) NOT NULL,
-    description_todo VARCHAR(500) NOT NULL,
+    title_task VARCHAR(100) NOT NULL,
+    description_task VARCHAR(500) NOT NULL,
     due_date DATE NOT NULL,
     is_completed BOOLEAN DEFAULT 0,
-    CONSTRAINT foreign_key_id_user FOREIGN KEY(id_user) REFERENCES USERS(id_user)
+    CONSTRAINT foreign_key_id_user FOREIGN KEY(id_user) REFERENCES USER(id_user)
 );
 
-INSERT INTO USERS VALUES
+INSERT INTO USER VALUES
 (NULL, 'Ewen', 'Buhot', 'ewen.buhot@gmail.com', 'ewenbuhot'),
 (NULL, 'Tom', 'Delangle', 'tom.delangle@gmail.com', 'tomdelangle'),
 (NULL, 'Thomas', 'Quero', 'thomas.quero@gmail.com', 'thomasquero');
 
-INSERT INTO TODOLIST VALUES
+INSERT INTO TASK VALUES
 (NULL, 1, 'Faire les courses', 'Acheter du lait, des œufs et du pain', '2022-04-10', 0),
 (NULL, 1, 'Terminer le rapport', 'Finaliser le rapport pour le projet ABC', '2022-04-15', 0),
 (NULL, 2, 'Appeler le médecin', 'Prendre rendez-vous pour un bilan de santé', '2022-04-12', 0),
